bc72f50d80290015a8e6801153408f87c80e5f8c
Oleg Zarevych <Oleg.Zarevych@gmail.com>
2018-10-09 00:20:13 +0300

    Add register logic

---
+++ CoreBB.Web/Controllers/UserController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using CoreBB.Web.Models;
+
+namespace CoreBB.Web.Controllers
+{
+    [Authorize]
+    public class UserController : Controller
+    {
+        private CoreBBContext _dbContext;
+
+        public UserController(CoreBBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [AllowAnonymous, HttpGet]
+        public async Task<IActionResult> Register()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return View();
+        }
+
+        [AllowAnonymous, HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                throw new Exception("Invalid registration information.");
+            }
+
+            model.Name = model.Name.Trim();
+            model.Password = model.Password.Trim();
+            model.RepeatPassword = model.RepeatPassword.Trim();
+
+            var targetUser = _dbContext.User
+                .SingleOrDefault(u => u.Name.Equals(model.Name, StringComparison.CurrentCultureIgnoreCase));
+
+            if (targetUser != null)
+            {
+                throw new Exception("User name already exists.");
+            }
+
+            if (!model.Password.Equals(model.RepeatPassword))
+            {
+                throw new Exception("Passwords are not identical.");
+            }
+
+            var hasher = new PasswordHasher<User>();
+            targetUser = new User { Name = model.Name, RegisterDateTime = DateTime.Now, Description = model.Description };
+            targetUser.PasswordHash = hasher.HashPassword(targetUser, model.Password);
+
+            if (_dbContext.User.Count() == 0)
+            {
+                targetUser.IsAdministrator = true;
+            }
+
+            await _dbContext.User.AddAsync(targetUser);
+            await _dbContext.SaveChangesAsync();
+
+            await LogInUserAsync(targetUser);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        private async Task LogInUserAsync(User user)
+        {
+            var claims = new List<Claim>();
+            claims.Add(new Claim(ClaimTypes.Name, user.Name));
+            if (user.IsAdministrator)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, Roles.Administrator));
+            }
+
+            var claimsIndentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            var claimsPrincipal = new ClaimsPrincipal(claimsIndentity);
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimsPrincipal);
+            user.LastLogInDateTime = DateTime.Now;
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
