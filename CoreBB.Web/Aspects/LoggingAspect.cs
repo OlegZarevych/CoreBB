89f49b723e24e201a8848ef889aeb849f22dda75
Oleg Zarevych <Oleg.Zarevych@gmail.com>
2018-10-17 01:05:32 +0300

    add PostSharp support

---
+++ CoreBB.Web/Aspects/LoggingAspect.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using PostSharp.Aspects;
+
+namespace CoreBB.Web.Aspects
+{
+    [Serializable]
+    public class LoggingAspect : OnMethodBoundaryAspect
+    {
+        public override void OnEntry(MethodExecutionArgs args)
+        {
+
+        }
+
+        public override void OnExit(MethodExecutionArgs args)
+        {
+
+        }
+    }
+}
